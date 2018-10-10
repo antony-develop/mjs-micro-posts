5e77aa6de44258de606c1e026c42146a6431878b
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2018-10-10 21:31:39 +1000

    add post removal functionality

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Add post click
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 function getPosts() {
     http.get(apiBaseUrl + '/posts')
     .then(data => ui.showPosts(data))
@@ -29,4 +32,22 @@ function submitPost() {
             getPosts();
         })
         .catch(err => console.log(err));
+}
+
+function deletePost(e) {
+    e.preventDefault();
+
+    const deleteLink = e.target.closest('.delete.card-link')
+    if (e.target.parentElement.classList.contains('delete')) {
+        const id = e.target.parentElement.dataset.id;
+
+        if (confirm('Are you sure')) {
+            http.delete(apiBaseUrl + '/posts/' + id)
+                .then(data => {
+                    ui.flashMessage('Post successfuly deleted', 'success');
+                    getPosts()
+                })
+                .catch(err => console.log(err));
+        }
+    }
 }
